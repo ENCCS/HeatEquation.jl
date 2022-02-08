89d1087ae40bcf70a798f1d1a91cdb40fe5c6b26
Kjartan Thor Wikfeldt <ktwikfeldt@gmail.com>
2022-02-08 23:19:30 +0100

    add untested evolve_gpu kernel

--- src/core.jl
+++ src/core.jl
@@ -15,7 +15,17 @@ function evolve!(curr::Field, prev::Field, a, dt)
     end
 end
 
+function evolve_gpu!(currdata, prevdata, dx2, dy2, a, dt)
+    nx, ny = size(currdata)    
+    j = blockIdx.x * blockDim.x + threadIdx.x
+    i = blockIdx.y * blockDim.y + threadIdx.y
 
+    if i > 1 && j > 1 && i < nx+2 && j < ny+2
+        xderiv = (prevdata[i-1, j] - 2.0 * prevdata[i, j] + prevdata[i+1, j]) / dx2
+        yderiv = (prevdata[i, j-1] - 2.0 * prevdata[i, j] + prevdata[i, j+1]) / dy2
+        currdata[i, j] = prevdata[i, j] + a * dt * (xderiv + yderiv)
+    end
+end
 
 """
     swap_fields!(curr::Field, prev::Field)
@@ -54,8 +64,11 @@ function simulate!(curr::Field, prev::Field, nsteps)
 
     for i = 1:nsteps
         # calculate new state based on previous state
-        evolve!(curr, prev, a, dt)
-
+        if typeof(curr.data) <: CuArray
+            @cuda (curr.nx, curr.ny) evolve_gpu!(curr.data, prev.data, curr.dx^2, curr.dy^2, a, dt)
+        else
+            evolve!(curr, prev, a, dt)
+        end
         # swap current and previous fields
         swap_fields!(curr, prev)
 
