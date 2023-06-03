59f71f4213f48a630e331dbba710cc5c97bb90e0
Kjartan Thor Wikfeldt <ktwikfeldt@gmail.com>
2023-06-03 22:56:05 +0200

    add threading

--- src/core.jl
+++ src/core.jl
@@ -6,11 +6,11 @@ field prev. a is the diffusion constant and dt is the largest
 stable time step.    
 """
 function evolve!(curr::Field, prev::Field, a, dt)
-    for j = 2:curr.ny+1
+    Threads.@threads for j = 2:curr.ny+1
         for i = 2:curr.nx+1
-            xderiv = (prev.data[i-1, j] - 2.0 * prev.data[i, j] + prev.data[i+1, j]) / curr.dx^2
-            yderiv = (prev.data[i, j-1] - 2.0 * prev.data[i, j] + prev.data[i, j+1]) / curr.dy^2
-            curr.data[i, j] = prev.data[i, j] + a * dt * (xderiv + yderiv)
+            @inbounds xderiv = (prev.data[i-1, j] - 2.0 * prev.data[i, j] + prev.data[i+1, j]) / curr.dx^2
+            @inbounds yderiv = (prev.data[i, j-1] - 2.0 * prev.data[i, j] + prev.data[i, j+1]) / curr.dy^2
+            @inbounds curr.data[i, j] = prev.data[i, j] + a * dt * (xderiv + yderiv)
         end 
     end
 end
